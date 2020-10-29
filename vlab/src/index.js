bcdcbe2af55ea4dd8d642a1e9b03eac736cbd98f
LikhithaG <likhithagaddi@gmail.com>
2020-10-29 19:48:06 +0530

    Added Theory page

--- vlab/src/index.js
+++ vlab/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import Cards from './Components/cardsUI';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cards />
   </React.StrictMode>,
   document.getElementById('root')
 );
