67e2476981c11ea372cd27f5945e6e5e8c0cb212
dev2803 <devansh.training1@gmail.com>
2020-11-01 23:25:33 +0530

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -18,4 +17,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
