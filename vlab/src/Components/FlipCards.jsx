a9ba7bc903bcdf42ae441410aec80d3b09ad050f
LikhithaG <likhithagaddi@gmail.com>
2020-11-02 13:38:20 +0530

    Added cards to Self-Evaluation

--- vlab/src/Components/FlipCards.jsx
+++ vlab/src/Components/FlipCards.jsx
@@ -1,23 +1,112 @@
-import React, { Component } from 'react';
-import FlipCard from './Flipcard.jsx';
+import React, { Component } from "react";
+import FlipCard from "./Flipcard.jsx";
 
 class FlipCards extends Component {
-	state = {};
-	render() {
-		return (
-			<div className="container-fluid">
-				<div className="row flipcard d-flex justify-content-center" style={{ marginTop: '8em' }}>
-					<FlipCard />
-				</div>
-				<div className="row flipcard d-flex justify-content-center">
-					<FlipCard />
-				</div>
-				<div className="row flipcard d-flex justify-content-center">
-					<FlipCard />
-				</div>
+  state = {
+    Questions: [
+      {
+        id:
+          "1. According to the COCOMO model, a project can be categorized into",
+      },
+      {
+        id:
+          "2. In Intermediate COCOMO model, Effort Adjustment Factor (EAF) is derived from the effort multipliers by",
+      },
+      { id: "3. Project metrics are estimated during which phase?" },
+      {
+        id:
+          "4. According to Halsetad's metrics, program length is given by the ",
+      },
+      { id: "5. Complete COCOMO considers a software as a" },
+      {
+        id:
+          "6. Consider you are developing a web application, which would make use of a lot of web services provided by Facebook, Google, Flickr. Would it be wise to make estimates for this project using COCOMO?",
+      },
+    ],
+    contents: [
+      {
+        option1: " 3 types",
+        option2: " 4 types ",
+        option3: "  5 types ",
+        option4: " No such categorization ",
+      },
+      {
+        option1: " Adding them ",
+        option2: " Multiplying them ",
+        option3: " Taking their weighted average ",
+        option4: " Considering their maximum ",
+      },
+      {
+        option1: " Feasibility study ",
+        option2: " Planning ",
+        option3: " Design ",
+        option4: " Development ",
+      },
+      {
+        option1: " Sum of total number of operators and operands ",
+        option2: " Sum of number of unique operators and operands ",
+        option3: " Total number of operators ",
+        option4: " Total number of operands ",
+      },
+      {
+        option1: " Homogeneous system ",
+        option2: " Heterogeneous system ",
+        option3: "",
+        option4: "",
+      },
+      {
+        option1: " Yes, of course ",
+        option2: " Not at all ",
+        option3: "",
+        option4: "",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="container-fluid" style={{ marginBottom: "1%" }}>
+        <div
+          className="row flipcard d-flex justify-content-center"
+          style={{ marginTop: "4em" }}
+        >
+          <FlipCard
+            name={this.state.Questions[0].id}
+            content={this.state.contents[0]}
+          />
+        </div>
+        <div className="row flipcard d-flex justify-content-center">
+          <FlipCard
+            name={this.state.Questions[1].id}
+            content={this.state.contents[1]}
+          />
+        </div>
+        <div className="row flipcard d-flex justify-content-center">
+          <FlipCard
+            name={this.state.Questions[2].id}
+            content={this.state.contents[2]}
+          />
+        </div>
+        <div className="row flipcard d-flex justify-content-center">
+          <FlipCard
+            name={this.state.Questions[3].id}
+            content={this.state.contents[3]}
+          />
 			</div>
-		);
-	}
+        <div className="row flipcard d-flex justify-content-center">
+          <FlipCard
+            name={this.state.Questions[4].id}
+            content={this.state.contents[4]}
+          />
+        </div>
+        <div className="row flipcard d-flex justify-content-center">
+          <FlipCard
+            name={this.state.Questions[5].id}
+            content={this.state.contents[5]}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FlipCards;
--- vlab/src/Components/Flipcard.jsx
+++ vlab/src/Components/Flipcard.jsx
@@ -1,69 +1,80 @@
-import React, { useState } from 'react';
-import ReactCardFlip from 'react-card-flip';
-import '../index.css';
-//import Button from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import "../index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./card-style.css";
 
-function Flipcard() {
-	const [ isFlipped, setIsFlipped ] = useState(false);
+function Flipcard(props) {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-	function handleClick() {
-		setIsFlipped(!isFlipped);
-	}
-	return (
-		<ReactCardFlip
-			isFlipped={isFlipped}
-			flipDirection="horizontal"
-			flipSpeedFrontToBack="1.2"
-			flipSpeedBacktToFront="1.2"
-		>
-			<div>
-				<div style={{ backgroundColor: 'white', height: 350, width: 500, justifyContent: 'center' }}>
-					<h3 style={{ paddingTop: 20, textAlign: 'left' }}>
-						According to the COCOMO model, a project can be categorized into{' '}
-					</h3>
-					<form name="self evaluation" id="self eval" style={{ paddingTop: 20, textAlign: 'left' }}>
-						<div class="radio">
-							<input type="radio" name="q1" value="3 types" />3 types
-							<br />
-							<br />
-							<input type="radio" name="q1" value="3 types" />4 types
-							<br />
-							<br />
-							<input type="radio" name="q1" value="3 types" />5 types
-							<br />
-							<br />
-							<input type="radio" name="q1" value="3 types" />None of these
-						</div>
-						<br />
-						<br />
-					</form>
-					<button
-						onClick={handleClick}
-						class="btn btn-success btn-lg"
-						style={{ paddingTop: 20, textAlign: 'left' }}
-					>
-						<strong style={{ paddingTop: 20, textAlign: 'left' }}>Submit</strong>
-					</button>
-				</div>
-			</div>
-			<div>
-				<div style={{ height: 350, width: 500 }}>
-					<p style={{ paddingTop: 20, justifyContent: 'center', textAlign: 'left' }}>
-						There are three - <br />
-						<br />Basic COCOMO Model, <br />
-						<br />
-						Intermediate COCOMO Model,<br />
-						<br />
-						Complete COCOMO Model{' '}
-					</p>
-					<h3 style={{ paddingTop: 20, textAlign: 'left' }}>This is the back of the card.</h3>
-					<button onClick={handleClick} style={{ paddingTop: 20, textAlign: 'left' }}>
-						<strong style={{ paddingTop: 20, textAlign: 'left' }}>Front flip</strong>
-					</button>
-				</div>
-			</div>
-		</ReactCardFlip>
-	);
+  function handleClick() {
+    setIsFlipped(!isFlipped);
+  }
+  function Options(props) {
+    const isContent = props.isContent;
+    if (isContent) {
+      return (
+        <div>
+          <input type="radio" name="q1" value="3 types" /> {isContent}
+          <br />
+          <br />
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+  return (
+    <ReactCardFlip
+      isFlipped={isFlipped}
+      flipDirection="horizontal"
+      flipSpeedFrontToBack="1.2"
+      flipSpeedBacktToFront="1.2"
+    >
+      <div className="question">
+        <div>
+				  <h3>{props.name}</h3>
+				  <br></br>
+          <form name="self evaluation" id="self eval">
+            <div class="radio">
+              <Options isContent={props.content.option1} />
+              <Options isContent={props.content.option2} />
+              <Options isContent={props.content.option3} />
+              <Options isContent={props.content.option4} />
+            </div>
+          </form>
+          <button
+            onClick={handleClick}
+            className="btn btn-success btn-lg float-left"
+          >
+            <strong className="text-center">Submit</strong>
+          </button>
+        </div>
+      </div>
+      <div className="question">
+        <div style={{ height: 350, width: 800 }}>
+          <p
+            style={{
+              paddingTop: 20,
+              justifyContent: "center",
+              textAlign: "left",
+            }}
+          >
+            There are three - <br />
+            <br />
+            Basic COCOMO Model, <br />
+            <br />
+            Intermediate COCOMO Model,
+            <br />
+            <br />
+            Complete COCOMO Model{" "}
+          </p>
+          <button onClick={handleClick} className="float-left btn btn-primary">
+            <strong>Go back to question</strong>
+          </button>
+        </div>
+      </div>
+    </ReactCardFlip>
+  );
 }
 export default Flipcard;
